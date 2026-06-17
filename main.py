b56493c31391d4259145b30de154b103559f5ad6
Eric Njogu <Consultant-Eric.Njogu@mcb.mu>
2026-06-17 18:09:00 +0200

    removing custom tool and using tavily directly

--- main.py
+++ main.py
@@ -6,34 +6,24 @@ from langchain_core.messages import HumanMessage
 from langchain_core.tools import Tool, tool
 from langchain_openai import ChatOpenAI
 import json
-from tavily import TavilyClient
-
-@tool
-def search(query: str) -> str:
-    """
-    a tool to search the web for information
-    Args:
-        query: the query to search for
-    Returns:
-        the search results
-    """
-    print(f"Searching the web for: {query}")
-    return tavily_client.search(query)
+from langchain_tavily import TavilySearch
 
 load_dotenv()
-tavily_client = TavilyClient()
 llm: ChatOpenAI = ChatOpenAI(model='gpt-5')
-tools: list[Tool] = [search]
+tools: list[Tool] = [TavilySearch()]
 agent = create_agent(llm, tools)
 
 def main():
     print("starting the search...")
-    result = agent.invoke({"messages": [HumanMessage(content="find 3 AI engineer jobs in Amsterdam")]})
-    # print(json.dumps(
-    #     {"messages": [m.model_dump() for m in result["messages"]]},
-    #     indent=2,
-    #     default=str,
-    # ))
+    result = agent.invoke({
+        "messages": [
+            HumanMessage(
+                content=(
+                    "find 2 recipes using precooked cornmeal for vegan meals"
+                )
+            )
+        ]
+    })
     print(result["messages"][-1].content)
 
 
