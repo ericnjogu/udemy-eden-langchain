f430a67639f8f2f576f797c092aebe0d1b864eda
Eric Njogu <Consultant-Eric.Njogu@mcb.mu>
2026-06-17 18:00:10 +0200

    integrating tavily to custom tool

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from langchain_core.messages import HumanMessage
 from langchain_core.tools import Tool, tool
 from langchain_openai import ChatOpenAI
 import json
-
-load_dotenv()
+from tavily import TavilyClient
 
 @tool
 def search(query: str) -> str:
@@ -19,20 +18,23 @@ def search(query: str) -> str:
         the search results
     """
     print(f"Searching the web for: {query}")
-    return "the weather in Bukavu is rainy"
+    return tavily_client.search(query)
 
-llm: ChatOpenAI = ChatOpenAI()
+load_dotenv()
+tavily_client = TavilyClient()
+llm: ChatOpenAI = ChatOpenAI(model='gpt-5')
 tools: list[Tool] = [search]
 agent = create_agent(llm, tools)
 
 def main():
     print("starting the search...")
-    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Bukavu?")]})
-    print(json.dumps(
-        {"messages": [m.model_dump() for m in result["messages"]]},
-        indent=2,
-        default=str,
-    ))
+    result = agent.invoke({"messages": [HumanMessage(content="find 3 AI engineer jobs in Amsterdam")]})
+    # print(json.dumps(
+    #     {"messages": [m.model_dump() for m in result["messages"]]},
+    #     indent=2,
+    #     default=str,
+    # ))
+    print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
